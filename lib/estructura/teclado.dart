50b7f0b4d0f619a19df4b6d387816f62618b0d67
hpjoss <hernandezpazjosemanuel@gmail.com>
2026-05-27 11:26:43 -0600

    creamos el archivo teclado.dart en el cual iran como los simbolos y numeros que vendrian en el teclado de nuestra calculadora

--- lib/estructura/teclado.dart
+++ lib/estructura/teclado.dart
@@ -10,7 +10,7 @@ class Teclado extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
     return Padding(
       padding: const EdgeInsets.only(
         left: 14,
